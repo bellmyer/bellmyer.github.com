976126bae8eb5e3fa70de1de090985f66064a848
Jaime Bellmyer <online@bellmyer.com>
2022-09-06 13:25:24 -0500

    try building conway again

---
+++ conway/precache-manifest.0c6eed5907f8699db449592c0541da8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2426279e0cafddefaa9401b479cd8dbc",
+    "url": "/conway/index.html"
+  },
+  {
+    "revision": "c55eb37b649b3df986f5",
+    "url": "/conway/static/css/2.bb4ba17c.chunk.css"
+  },
+  {
+    "revision": "387080f364a26dce2684",
+    "url": "/conway/static/css/main.7604df3a.chunk.css"
+  },
+  {
+    "revision": "c55eb37b649b3df986f5",
+    "url": "/conway/static/js/2.99f1936c.chunk.js"
+  },
+  {
+    "revision": "387080f364a26dce2684",
+    "url": "/conway/static/js/main.ac1e652f.chunk.js"
+  },
+  {
+    "revision": "820a45e2721fe91ad969",
+    "url": "/conway/static/js/runtime~main.e57dd940.js"
+  }
+]);
